d32a44c02ceae7891bf9a3ec9db62e5649eeb3ba
userdefined111 <kapilsoni1883@gmail.com>
2025-09-02 14:17:50 +0530

    2nd trial

--- day1.cpp
+++ day1.cpp
@@ -5,3 +5,11 @@ int main() {
     cout << "Hello, World!" << endl;
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
