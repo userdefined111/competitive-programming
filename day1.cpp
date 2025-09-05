c70b69a7a23c8d6f7253321de14f020e150f4ae7
userdefined111 <kapilsoni1883@gmail.com>
2025-09-05 16:17:40 +0530

    new work

--- day1.cpp
+++ day1.cpp
@@ -1,14 +1,5 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
-
-#include <iostream>
-using namespace std;
-
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
